f9d350408221495bc5ca0358ebd3bcd1f0853773
jorgesanabria <jorge.adrian.sanabria@gmail.com>
2025-07-03 05:02:51 -0300

    Fix JoinNode constructor and on condition handling

--- DBAL/QueryBuilder/Node/JoinNode.php
+++ DBAL/QueryBuilder/Node/JoinNode.php
@@ -2,6 +2,7 @@
 namespace DBAL\QueryBuilder\Node;
 
 use DBAL\QueryBuilder\MessageInterface;
+use DBAL\QueryBuilder\Message;
 
 class JoinNode extends NotImplementedNode
 {
@@ -12,22 +13,24 @@ class JoinNode extends NotImplementedNode
 	protected $table;
 	protected $type;
 	protected $on = [];
-	public function __construct($table, $type = JoinNode::INNER_JOIN, array $on = [])
-	{
-		$this->table = $table;
-		$this->table = $type;
-		foreach ($on as $filter)
-			$this->on[] = new FilterNode($on);
-	}
-	public function send(MessageInterface $message)
-	{
-		$msg = new Message($message->type(), sprintf('%s %s', $this->type, $this->table));
-		if (sizeof($this->on) > 0) {
-			foreach ($this->on as $filter)
-				$msg = $filter->send($msg);
-		}
-		return $message->join($msg);
-	}
+        public function __construct($table, $type = JoinNode::INNER_JOIN, array $on = [])
+        {
+                $this->table = $table;
+                $this->type  = $type;
+                foreach ($on as $filter)
+                        $this->on[] = new FilterNode($filter);
+        }
+        public function send(MessageInterface $message)
+        {
+                $msg = new Message($message->type(), sprintf('%s %s', $this->type, $this->table));
+                if (sizeof($this->on) > 0) {
+                        $onMsg = new Message($message->type());
+                        foreach ($this->on as $filter)
+                                $onMsg = $filter->send($onMsg);
+                        $msg = $msg->join($onMsg->insertBefore('ON'));
+                }
+                return $message->join($msg);
+        }
 	public function __clone()
 	{
 		foreach ($this->on as $key=>$node)
