9d77ba68632be41ec6edd13594636c34e7c380a2
jorgesanabria <jorge.adrian.sanabria@gmail.com>
2025-07-04 14:11:36 -0300

    Add attribute class docs

--- DBAL/Attributes/IntegerType.php
+++ DBAL/Attributes/IntegerType.php
@@ -1,5 +1,9 @@
 <?php
 namespace DBAL\Attributes;
 use Attribute;
+
+/**
+ * Ensures the decorated property contains an integer value.
+ */
 #[Attribute(Attribute::TARGET_PROPERTY)]
 class IntegerType {}
--- DBAL/Attributes/MaxLength.php
+++ DBAL/Attributes/MaxLength.php
@@ -1,8 +1,16 @@
 <?php
 namespace DBAL\Attributes;
 use Attribute;
+
+/**
+ * Restricts a string property to a maximum length.
+ */
 #[Attribute(Attribute::TARGET_PROPERTY)]
 class MaxLength {
     public int $length;
+
+    /**
+     * @param int $length Allowed maximum number of characters
+     */
     public function __construct(int $length) { $this->length = $length; }
 }
